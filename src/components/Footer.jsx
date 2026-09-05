34fca9b52013c5a742889c2bc08331113598c9bf
RealSolutionsxZ2H <z2hrealsolutions@gmail.com>
2026-09-05 15:49:47 +0530

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer({ variant = 'tv' }) {
-  return (
-    <div className={variant === 'tv' ? 'tv-footer' : 'phone-footer'}>
-      Designed and Developed by Z2HxRealSolutions
-    </div>
-  )
-}
--- src/components/Logo.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Logo({ height = 56 }) {
-  return <img src="/dnl-logo.jpg" alt="DNL - DUPR Night League" style={{ height, borderRadius: 8 }} />
-}
